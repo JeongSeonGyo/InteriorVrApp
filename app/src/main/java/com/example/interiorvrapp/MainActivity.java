c09df0fa475fee85d91eebb0dd3d03d8f70ae7e7
Jeong Seon Gyo <starmichelle@ajou.ac.kr>
2017-05-14 16:46:49 +0900

    urlConnection update

--- app/src/main/java/com/example/interiorvrapp/MainActivity.java
+++ app/src/main/java/com/example/interiorvrapp/MainActivity.java
@@ -1,15 +1,74 @@
 package com.example.interiorvrapp;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText idInput, passwordInput;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Boolean loginChecked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a = 1;
-        int b = -1;
+        String test = "http://13.124.5.176/phpinfo.php";   //"http://localhost/MediumServer/SelectAllPost.php"
+        URLConnector task = new URLConnector(test);
+
+        task.start();
+
+        try{
+            task.join();
+            System.out.println("waiting... for result");
+        }catch(InterruptedException e){
+
+        }
+        String result = task.getResult();
+        System.out.println("Success");
+        System.out.println(result);
+
+        idInput = (EditText) findViewById(R.id.ID);
+        passwordInput = (EditText) findViewById(R.id.password);
+        Button signupbtn = (Button) findViewById(R.id.Signupbtn);
+        Button loginbtn = (Button) findViewById(R.id.loginbtn);
+
+        String id = idInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        //when you click sign up button
+        signupbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //go to sign up
+                Intent signup = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(signup);
+
+            }
+        });
+
+        //when you click login
+        loginbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //go to vr
+            }
+
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/interiorvrapp/SignupActivity.java
@@ -0,0 +1,97 @@
+package com.example.interiorvrapp;
+
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by 쿄 on 2017.05.12 0012.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+    Boolean validation = false;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        Button joinbtn = (Button) findViewById(R.id.join);
+        Button cancelbtn = (Button) findViewById(R.id.cancel);
+
+        //when you click join button
+        joinbtn.setOnClickListener(new View.OnClickListener(){
+            EditText newnameInput = (EditText) findViewById(R.id.newName);
+            EditText newidInput = (EditText) findViewById(R.id.newID);
+            EditText newpwInput = (EditText) findViewById(R.id.newPw);
+            EditText newpwcheckInput = (EditText) findViewById(R.id.newPwCheck);
+
+            @Override
+            public void onClick(View v){
+                if (newnameInput.getText().toString().length() == 0) {
+                    Toast.makeText(SignupActivity.this, "Enter Name", Toast.LENGTH_LONG).show();
+                    newnameInput.requestFocus();
+                    return;
+                }
+                if (newidInput.getText().toString().length() == 0) {
+                    Toast.makeText(SignupActivity.this, "Enter ID", Toast.LENGTH_LONG).show();
+                    newidInput.requestFocus();
+                    return;
+                }
+
+                if (newpwInput.getText().toString().length() == 0) {
+                    Toast.makeText(SignupActivity.this, "Enter Password", Toast.LENGTH_LONG).show();
+                    newpwInput.requestFocus();
+                    return;
+                }
+
+                if (newpwcheckInput.getText().toString().length() == 0) {
+                    Toast.makeText(SignupActivity.this, "Enter Password Again", Toast.LENGTH_LONG).show();
+                    newpwcheckInput.requestFocus();
+                    return;
+                }
+
+                //Check validation btw pw and pwchekck
+                if (!pwvalidation(newpwInput.getText().toString(), newpwcheckInput.getText().toString())) {
+                        Toast.makeText(SignupActivity.this, "Retype your password", Toast.LENGTH_LONG).show();
+                        newpwcheckInput.setText("");
+                        newpwcheckInput.requestFocus();
+                        return;
+                    } else {
+                        //db 저장
+                        Toast.makeText(SignupActivity.this, "Confirm", Toast.LENGTH_LONG).show();
+                        Toast.makeText(SignupActivity.this, "Complete & Back to Login page", Toast.LENGTH_LONG).show();
+                        finish();
+                }
+            }
+        });
+
+        cancelbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                Toast.makeText(SignupActivity.this,"Cancel & Back to Login page",Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+
+    }
+
+    private boolean pwvalidation(String pw, String pwcheck){
+        if(pwcheck.equals(pw)){
+            //same as password that user input
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+}
