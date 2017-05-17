c04b87fe51c06209fdc0891e3603560f5f07fef9
KwangBae <kblee1022@gmail.com>
2017-05-17 17:08:11 +0900

    failed login

---
+++ app/src/main/java/com/example/interiorvrapp/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.interiorvrapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by KwangBae on 2017-05-16.
+ */
+
+public class LoginActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+}
